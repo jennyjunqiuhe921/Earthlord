aafa9bd34feeff91b767399e6d4c7ce536305b84
tuotuo <1640957909@qq.com>
2025-12-23 22:31:23 +0800

    Initial Commit

---
+++ Earthlord/EarthlordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthlordApp.swift
+//  Earthlord
+//
+//  Created by gong on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthlordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
